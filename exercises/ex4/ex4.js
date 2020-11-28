6cf5438a2c1701a33270f0176c1db1f5956247f7
mccor2000 <roccm0200@gmail.com>
2020-11-28 21:14:55 +0700

    feat: finished ex4

---
+++ exercises/ex4/ex4.js
@@ -0,0 +1,42 @@
+function fakeAjax(url, cb) {
+  var fake_responses = {
+    file1: "The first text",
+    file2: "The middle text",
+    file3: "The last text",
+  };
+  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000;
+
+  console.log("Requesting: " + url);
+
+  setTimeout(function () {
+    cb(fake_responses[url]);
+  }, randomDelay);
+}
+
+function output(text) {
+  console.log(text);
+}
+
+// **************************************
+// The old-n-busted callback way
+
+function getFile(file) {
+  return new Promise(function (resolve) {
+    fakeAjax(file, resolve);
+  });
+}
+
+["file1", "file2", "file3"]
+  .map(function (filename) {
+    return getFile(filename);
+  })
+  .reduce((chain, currentPr) => {
+    return chain
+      .then(() => {
+        return currentPr;
+      })
+      .then(output);
+  }, Promise.resolve())
+  .then(() => {
+    console.log("Complete!");
+  });
